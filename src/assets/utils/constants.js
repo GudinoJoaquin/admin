f883033e58536806139cc22ae6122cc8c00357f5
FacundoCien <130766359+FacundoCien@users.noreply.github.com>
2024-06-04 17:16:59 -0400

    1111

--- src/assets/utils/constants.js
+++ src/assets/utils/constants.js
@@ -15,13 +15,14 @@ export function updateUsuario(newUsuario) {
 export const RUTAS = {
   home: "/",
   crear: "/crear",
-  enviar: "https://anuncios.vercel.app/admin/enviar-anuncio",
-  enviarEditar: "https://anuncios.vercel.app/admin/editar-anuncio",
+  enviar: "https://anuncios.vercel.app/admin/enviar-anuncio?apiKey=v",
+  enviarEditar: "https://anuncios.vercel.app/admin/editar-anuncio?apiKey=v",
   editar: "/editar",
-  eliminar: "https://anuncios.vercel.app/eliminar-anuncio?id=",
+  eliminar: "https://anuncios.vercel.app/eliminar-anuncio?id=&apiKey=v",
   login: "/login",
   configurar: "/configurar",
 };
+
 
 export const COOKIE_INFO = {
   name: "SVZHJm6L6RB5t6VeLESJHv33qYPY1XXfPrpU6bavKyBjPvU5EXKyp3pxSg4vCgmUuZ3Jmw5rSey2sN7T62fcLMXZz5EJ1KVKv1wwpm6cCdBBN8auYhKeL9pgCgmUuZ3Jmw5rSey2sN7T62fcLMXZz5EJ1KVKv1wwpm6cCdBBN8auYhKeL9pgDqW5Fu2RmcgjjZXsQcds678ZbBdK3jNe5gzNS7k3KYxpRZQGeDyG7N3xT13ZTDZCDxfEsGcRLUbrNFLBQAwmzz71T1M8QzkPy4H4Uw4PYfXNv7f4XyfvEZu967BaXqqKQzfzZ6Hsu6qMgcGHqvDrQhgQ173CSBaafj8MahNd327tbqyeQrjgzzjwpanMn2KKqQ1QQY1u4cDRMfdQp4WqZP53YE8CZBrZ9rEJzVeyntCFEsWVmFNYpkwmCWT1brRWF1QER43yyQCQqhHGWFLZEdGGya5SNNP6e4QUCFdcPPnd7GfKdK1hjtvTe418b4ktc8yEfuUmCsNkFkQdb1mqk5jTNwVTcQhmVVvgjU48tQWQB7RKrQBWGCBxjkT2zqN93gcvTeDuGSLe1BszNMSyDFETQmpSBhSzUUp2DyLth1GzTKw4avjTAkavWbUR2yCUkFTN1LtgWWNaxj685QHwxHTDJfjB7KFTsRny8CAjkyxqLCLTegrwXQC7vMENFQFZ2UjGBYneFaRXHeCgA7KbE3k2LTUf",
